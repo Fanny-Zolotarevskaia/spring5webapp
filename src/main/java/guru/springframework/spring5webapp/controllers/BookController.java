b8692573297657f36ae41558da8b1c803e00c2a3
Fanny Zolotarevskaia <99657593+Fanny-Zolotarevskaia@users.noreply.github.com>
2022-09-27 16:49:22 +0200

    Created a Controller for book

---
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -0,0 +1,26 @@
+package guru.springframework.spring5webapp.controllers;
+
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Fanny on 09/27/22.
+ */
+@Controller
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+        model.addAttribute("books", bookRepository.findAll()); //gets a list of books present in the repository
+
+        return "books";
+    }
+}
